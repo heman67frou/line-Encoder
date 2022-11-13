24366e510242892372d795b3b47f0086604cf73e
Hemanshu Sharma <Hemanshu00001@gmail.com>
2022-11-13 18:30:39 +0530

    introduce code for palindromic subsequence and improve code

--- plotter.py
+++ plotter.py
@@ -1,3 +1,6 @@
+import random
+import sys
+from traceback import print_tb
 import turtle
 import tkinter as tk
 
@@ -18,22 +21,24 @@ class NRZ_I:
 
     def draw(self):
         t.sety(self.logic_high) 
+        count = 0
         for i in self.signal:
             if i == '0':
                 self.zero()
             elif i == '1':
-                self.one()
+                count += 1
+                self.one(count)
 
     def zero(self):
         t.forward(self.distance)
 
-    def one(self):
-        posx, posy = t.pos()
-        if self.logic_low - 1 < posy < self.logic_low + 1:
-            t.sety(self.logic_high)
-        elif self.logic_high - 1 < posy < self.logic_high + 1:
+    def one(self, count):
+        if count%2 == 1:
             t.sety(self.logic_low)
-        t.forward(self.distance)
+            t.forward(self.distance)
+        else:
+            t.sety(self.logic_high)
+            t.forward(self.distance)
 
 
 class NRZ_L:
@@ -57,41 +62,6 @@ class NRZ_L:
 
     def one(self):
         t.sety(self.logic_low)
-        t.forward(self.distance)
-
-
-class RZ:
-
-    def __init__(self, signal: str):
-        self.signal = signal
-        self.logic_high = 50
-        self.logic_low = -50
-        self.distance = 50
-        self.base = 0
-
-    def draw(self):
-        for i in self.signal:
-            if i == '0':
-                self.zero()
-            elif i == '1':
-                self.one()
-
-    def zero(self):
-        t.sety(self.logic_low)
-        t.forward(self.distance)
-        t.sety(self.base)
-        setTurtle(*invisiline)
-        t.write('0', False, 'right', ("Arial", 12, "normal"))
-        setTurtle(*default_settings)
-        t.forward(self.distance)
-
-    def one(self):
-        t.sety(self.logic_high)
-        t.forward(self.distance)
-        t.sety(self.base)
-        setTurtle(*invisiline)
-        t.write('1', False, 'right', ("Arial", 12, "normal"))
-        setTurtle(*default_settings)
         t.forward(self.distance)
 
 
@@ -312,18 +282,75 @@ def setTurtle(size, colour, speed, visibility):
     if not visibility:
         t.hideturtle()
 
+def rand_key(p, zero_size):
+    key = ""
+    if p == zero_size:
+        for i in range(p):
+            key += "0"
+    else:
+        k = random.randint(0, p-zero_size+1)
+        for i in range(p):
+            if i >= k and i < k+zero_size:
+                key += "0"
+            else:
+                temp = str(random.randint(0, 1))
+                key += temp
+    return(key)
+
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
+
+def longestPalindromicSequence(st, n):
+    rev = reverse(st)
+    P = [[0 for i in range(n+1)] for j in range(n+1)]
+    for i in range(n+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                P[i][j] = 0
+            elif st[i-1] == rev[j-1]:
+                P[i][j] = P[i-1][j-1] + 1
+            else:
+                P[i][j] = max(P[i-1][j], P[i][j-1])
+    str = ""
+    i = n
+    j = n
+    while i > 0 and j > 0:
+        if st[i-1] == rev[j-1]:
+            str += st[i-1]
+            i -= 1
+            j -= 1
+        elif P[i-1][j] > P[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    str = str[::-1]
+    print("Longest Paindromic Sub-Sequence of " + st + " is " + str)
+
 if __name__=="__main__":
-    print('Input Signal to be plotted (1s and 0s)')
-    signal = input()
-    print('\nEncode in format:\n1. NRZ-I\n2. NRZ-L\n3. RZ\n4. Manchester\n5. Diff Manchester\n6. AMI\n')
+    print('\nChoose Encoding Format:\n1. NRZ-I\n2. NRZ-L\n3. Manchester\n4. Diff Manchester\n5. AMI\n')
     encoding = int(input())
-    if encoding == 6:
-        print('\nChoose scrambling format:\n1. HDB3\n2. B8ZS\n')
+    if encoding == 5:
+        print('\nChoose Scrambling format:\n1. HDB3\n2. B8ZS\n')
         encoding2 = int(input())
         if encoding2 == 1:
-            encoding = 6
+            encoding = 5
         else:
-            encoding = 7
+            encoding = 6
+
+    print('Length of binary string required')
+    size = int(input())
+    zero_size = 0
+    if encoding == 5:
+        print('Total No Of subsequent 0s')
+        zero_size = int(input())
+    if zero_size > size:
+        print('Total length of string is less than total 0s required')
+        sys.exit(0)
+    signal = rand_key(size, zero_size)
+    print('Your generated signal : ' + signal)
 
     root = tk.Tk()
     root.title('Line Encoder')
@@ -334,8 +361,8 @@ if __name__=="__main__":
     len_X, len_Y = 5000, 350
     default_settings = (2, 'green', 'slowest', False)
     invisiline = (1, 'black', 'fastest', False)
-    map = {1: NRZ_I(signal), 2: NRZ_L(signal), 3: RZ(signal), 4: Manchester(signal),
-        5: diff_Manchester(signal), 6: AMI_HDB3(signal), 7: AMI_B8ZS(signal)}
+    map = {1: NRZ_I(signal), 2: NRZ_L(signal), 3: Manchester(signal),
+        4: diff_Manchester(signal), 5: AMI_HDB3(signal), 6: AMI_B8ZS(signal)}
 
     screen = turtle.TurtleScreen(cv)
     screen.screensize(len_X, len_Y)
@@ -346,3 +373,7 @@ if __name__=="__main__":
     map[encoding].draw()
 
     root.mainloop()
+    x = reverse(signal)
+    longestPalindromicSequence(signal, size)
+
+    sys.exit(0)
